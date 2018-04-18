bc8eea54107a23a1d92ef42f8ccd41dcce17db64
Dan Klinga <dklinga@Dans-MacBook-Pro.local>
2018-04-18 18:29:27 -0400

    wed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,3 +13,22 @@ var connection = mysql.createConnection({
     password: "",
     database: "bamazon"
   });
+
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    queryItems();
+  });
+
+
+  //List query
+  function queryItems() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+     // console.log(res);
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+      }
+      console.log("-----------------------------------");
+    });
+  }
